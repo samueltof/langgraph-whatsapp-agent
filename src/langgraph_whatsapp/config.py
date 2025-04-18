434e897b06b0502d6994abd76de343132e0b761d
“lgesuellip” <lgesuellipinto@uade.edu.ar>
2025-04-18 00:10:14 -0300

    fix images

--- src/langgraph_whatsapp/config.py
+++ src/langgraph_whatsapp/config.py
@@ -7,3 +7,4 @@ LANGGRAPH_URL = environ.get("LANGGRAPH_URL")
 ASSISTANT_ID = environ.get("LANGGRAPH_ASSISTANT_ID", "agent")
 CONFIG = environ.get("CONFIG") or "{}"
 TWILIO_AUTH_TOKEN = environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_ACCOUNT_SID = environ.get("TWILIO_ACCOUNT_SID")
